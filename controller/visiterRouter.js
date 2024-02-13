763ba01e813269eb452c9342bc2c0a8748fb0e2d
aswathyappukuttan2001 <aswathyappukuttan2019>
2024-02-13 23:04:14 +0530

    updated

---
+++ controller/visiterRouter.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const router=express.Router()
+const visitorModels = require("../models/visiterModels")
+
+
+    
+router.get("/view",async(req,res)=>{
+    let data1=await visitorModels.find()
+    res.json(data1)
+    })
+
+router.post("/add",async(req,res)=>{
+    let data = req.body
+    let visitor=new visitorModels(data)
+    let result=await visitor.save()
+    res.json({
+
+       status:"success"
+      })
+})
+
+
+
+
+module.exports=router
